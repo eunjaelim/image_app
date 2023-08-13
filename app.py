4378f87a7e278a6c208d7c28919fb33a68356446
Eunjnae Lim <99618295+eunjaelim@users.noreply.github.com>
2023-08-13 12:08:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ if option == '글자추출':
             
             df = pd.DataFrame(result_text, columns=['Extracted Text'])
             edited_df = st.data_editor(df, num_rows="dynamic")
-            # st.dataframe(edited_df)
+            # st.dataframe(df)
            
             
             
