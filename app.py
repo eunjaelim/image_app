952fb7afebec36cb257244b39c9961abb5480de4
Eunjnae Lim <99618295+eunjaelim@users.noreply.github.com>
2023-08-13 12:41:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -75,11 +75,11 @@ def convert_df(df):
      return df.to_csv().encode('utf-8')
 
 csv = convert_df(df)
-        st.download_button(
-               label="Download data as CSV",
-               data=csv,
-               file_name='sample_df.csv',
-               mime='text/csv',
+st.download_button(
+     label="Download data as CSV",
+     data=csv,
+     file_name='sample_df.csv',
+     mime='text/csv',
            )
 
 
