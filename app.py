8d38064a28e3fcb42ae3c22c512db06d09f3706b
Eunjnae Lim <99618295+eunjaelim@users.noreply.github.com>
2023-08-13 12:34:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,15 @@ if option == '글자추출':
            csv = convert_df(df)
 
            #adding a download button to download csv file
-     
+
+           @st.cache
+   
+           def convert_df(df):
+           
+               # IMPORTANT: Cache the conversion to prevent computation on every rerun
+           
+               return df.to_csv().encode('utf-8')
+        
            st.download_button(
                label="Download data as CSV",
            
