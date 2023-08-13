9a24ea3f29d664e0fe0d42b1128cc6d2ac0065a1
Eunjnae Lim <99618295+eunjaelim@users.noreply.github.com>
2023-08-13 12:20:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -64,20 +64,7 @@ if option == '글자추출':
             edited_df = st.data_editor(df)
             # st.dataframe(df)
 
-            @st.experimental_memo
-            def convert_df(df):
-               return df.to_csv(index=False).encode('utf-8')
-            
-            
-               csv = convert_df(df)
-            
-               st.download_button(
-                    "Press to Download",
-                    csv,
-                    "file.csv",
-                    "text/csv",
-                    key='download-csv'
-                 )
+            st.download_button('Download CSV', df, 'text/csv')
 
            
             
