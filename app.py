118f15d6b688378adb8401ddcd36c3bf5e4852ee
Eunjnae Lim <99618295+eunjaelim@users.noreply.github.com>
2023-08-13 12:51:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -75,24 +75,6 @@ if option == '글자추출':
                  mime='text/csv',
                        )
 
-
-
-#  #CSV 다운로드 버튼 추가히기
-
-          
-# @st.cache   
-# def convert_df(df):
-#      return df.to_csv().encode('utf-8')
-
-# csv = convert_df(df)
-# st.download_button(
-#      label="Download data as CSV",
-#      data=csv,
-#      file_name='sample_df.csv',
-#      mime='text/csv',
-#            )
-
-
    
 
 
